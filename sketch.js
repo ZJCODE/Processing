41b4a5903cf06de9170c29d149d4d347036fa273
ZhangJun <zhangjun310@live.com>
2024-07-03 18:32:39 +0800

    update

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 // p5 js template
 
+let in_index = true;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
-function draw() {
-  background("#101010");
+function index_page() {
   translate(width / 2, height / 2);
   textFont("Quicksand");
   textSize(height / 8);
@@ -18,22 +19,41 @@ function draw() {
   textFont("Noto Sans Symbols 2");
   fill(180);
   textSize(height / 15);
-  text("🢣", width / 2.3, height / 2.3);
+  text("🢣", width / 2.5, height / 2.3);
+}
+
+function another_page() {
+  fill(220);
+  text("Click to go back", width / 2, height / 10);
+  textFont("Noto Sans Symbols 2");
+  fill(180);
+  textSize(height / 15);
+  text("🢢", width / 10, height / 1.07);
+}
+
+function draw() {
+  background("#101010");
+  if (in_index) index_page();
+  else another_page();
 }
 
 function mousePressed() {
   // 检查鼠标位置是否在文字区域内
-  let d = dist(
-    mouseX,
-    mouseY,
-    width / 2 + width / 2.3,
-    height / 2 + height / 2.3
-  );
-  if (d < height / 10) {
-    window.open(
-      "https://zjcode.github.io/Processing/Case/index.html",
-      "_blank"
-    ); // 打开链接
+  if (in_index) {
+    let d = dist(
+      mouseX,
+      mouseY,
+      width / 2 + width / 2.5,
+      height / 2 + height / 2.3
+    );
+    if (d < height / 10) {
+      in_index = false;
+    }
+  } else {
+    let d = dist(mouseX, mouseY, width / 10, height / 1.07);
+    if (d < height / 10) {
+      in_index = true;
+    }
   }
 }
 
