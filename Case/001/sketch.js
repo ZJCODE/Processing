0e9a99906820994290a7ffcb8bed08d6cf1d1d6b
ZhangJun <zhangjun310@live.com>
2024-07-03 14:47:23 +0800

    update

--- Case/001/sketch.js
+++ Case/001/sketch.js
@@ -1,15 +1,72 @@
 // p5 js template
 
+let grids = [];
+let num = 15;
+
+function init() {
+  grids = [];
+  for (let i = 0; i < num; i++) {
+    let x = Math.floor(random(width));
+    let y = Math.floor(random(height));
+    let w = Math.floor(random(1, 5));
+    let g = Math.floor(random(10, 50));
+    let size1 = Math.floor(random(1, 10));
+    let size2 = Math.floor(random(1, 10));
+    let r = random(PI);
+    grids.push(new Grid(x, y, w, g, size1, size2, r));
+  }
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  init();
 }
 
 function draw() {
   background(0);
-  translate(width / 2, height / 2);
-  textSize(32);
-  textAlign(CENTER, CENTER);
-  fill(255);
-  text("Hello World", 0, 0);
+  for (let grid of grids) {
+    grid.show();
+    // text show grid info
+    // push();
+    // translate(grid.x, grid.y);
+    // fill(255);
+    // noStroke();
+    // text(`w:${grid.w} g:${grid.g} s1:${grid.size1} s2:${grid.size2}`, 0, 0);
+    // pop();
+  }
 }
-//
+
+// click to reset
+function mousePressed() {
+  init();
+}
+
+// 构建一个 Grid 类
+// 用于在以 (x,y) 为中心 使用宽度为 w 的 横竖线条构建间隔为 g 的 size1*size2 的网格 并旋转 r 角度
+
+class Grid {
+  constructor(x, y, w, g, size1, size2, r) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.g = g;
+    this.size1 = size1;
+    this.size2 = size2;
+    this.r = r; // new property for rotation angle
+  }
+
+  show() {
+    push();
+    translate(this.x, this.y);
+    rotate(this.r); // rotate by the specified angle
+    stroke(255, 100);
+    strokeWeight(this.w);
+    for (let i = -this.size1; i <= this.size1; i++) {
+      line(-this.size2 * this.g, i * this.g, this.size2 * this.g, i * this.g);
+    }
+    for (let i = -this.size2; i <= this.size2; i++) {
+      line(i * this.g, -this.size1 * this.g, i * this.g, this.size1 * this.g);
+    }
+    pop();
+  }
+}
--- sketch.js
+++ sketch.js
@@ -13,5 +13,18 @@ function draw() {
   text("Hello World", 0, -50);
   textSize(32);
   text("Something Interesting Gonna Happen Here", 0, 50);
+  textSize(16);
+  text("Click Me", 0, 150); // 假设这是你想要点击的链接
 }
-//
+
+function mousePressed() {
+  // 检查鼠标位置是否在“Click Me”文字区域内
+  let d = dist(mouseX, mouseY, width / 2, height / 2 + 150);
+  if (d < 50) {
+    // 假设100是根据文字大小和间距大致估算的点击区域半径
+    window.open(
+      "https://zjcode.github.io/Processing/Case/index.html",
+      "_blank"
+    ); // 打开链接
+  }
+}
