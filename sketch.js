f703778d37e20ce9d9154221b20ce9d8bed0ac64
ZhangJun <zhangjun310@live.com>
2024-07-04 10:32:46 +0800

    update

--- sketch.js
+++ sketch.js
@@ -1,9 +1,75 @@
 // p5 js template
 
 let in_index = true;
+let particles = [];
+let particles_num = 20;
+let mVel = 1;
+
+class Particle {
+  constructor() {
+    this.pos = createVector(random(width), random(height));
+    this.vel = createVector(random(-mVel, mVel), random(-mVel, mVel));
+  }
+
+  update() {
+    this.pos.add(this.vel);
+    this.bounce();
+  }
+
+  check(val) {
+    const dist = p5.Vector.dist(val, this.pos);
+    return dist < 100;
+  }
+
+  render() {
+    fill(150);
+    textSize(min(width, height) / 60);
+    text("◉", this.pos.x, this.pos.y);
+  }
+
+  bounce() {
+    if (this.pos.x > width || this.pos.x < 0) {
+      this.vel.x *= -1;
+    }
+    if (this.pos.y > height || this.pos.y < 0) {
+      this.vel.y *= -1;
+    }
+  }
+}
+
+function linked_dots() {
+  for (let i = 0; i < particles.length; i++) {
+    particles[i].update();
+    particles[i].render();
+
+    for (let y = 0; y < particles.length; y++) {
+      if (
+        particles[i].check(particles[y].pos) &&
+        particles[i] != particles[y]
+      ) {
+        strokeWeight(0.2);
+        stroke(255, 50); // 增加线的透明度
+        line(
+          particles[i].pos.x,
+          particles[i].pos.y,
+          particles[y].pos.x,
+          particles[y].pos.y
+        );
+      }
+    }
+  }
+}
+
+function init_dot() {
+  particles = [];
+  for (let i = 0; i < particles_num; i++) {
+    particles.push(new Particle());
+  }
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  init_dot();
 }
 
 // 计算箭头位置的函数
@@ -47,8 +113,12 @@ function another_page() {
 
 function draw() {
   background("#101010");
-  if (in_index) index_page();
-  else another_page();
+  if (in_index) {
+    index_page();
+    linked_dots();
+  } else {
+    another_page();
+  }
 }
 
 function mousePressed() {
@@ -78,4 +148,5 @@ function mousePressed() {
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+  init_dot();
 }
